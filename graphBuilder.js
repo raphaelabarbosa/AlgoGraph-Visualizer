19fc47baa5d91b6cb40ef90c9e5a589eb575d1aa
Raphaela Barbosa <raphaelasb07@gmail.com>
2026-05-31 11:35:19 -0300

    Graph Builder base - Input processing update

--- graphBuilder.js
+++ graphBuilder.js
@@ -19,26 +19,36 @@ function draw(){
 
 function graph_draw(){
     background(220);
-    let arestas = Input.value().split(",");
+    //1.Receber número de vértice (n)
+    //2.Receber vértices
+    //3.Fazer compressão de coordenada com map (Compr.,Original)
+    //4.Construir vetor de adj com compr.
+    //5.Desenhar grafo consultando no map
 
-    let x = 50;
-    let y = 50;
-    let diametro = 25;
-    for(let i = 0; i < arestas.length; i++){
-            let par = arestas[i].split("-");
-            for(let v of par){
-            //Circulo
-            fill(255); 
-            stroke(0); 
-            circle(x, y, diametro);
-            //Texto
-            fill(0); 
-            textSize(10);
-            textAlign(CENTER, CENTER); 
-            text(v.trim(), x, y);
-            x+= 50;
-        }
-        x = 50;
-        y += 50;
+    let graph_input = Input.value().split(","); //Separando valores do input em um array: string "n, v1-v2, v3-v2,...""
+    
+    let n = parseInt(graph_input[0].trim()); //Número de vértices
+    //console.log(n);
+    for(let i = 1; i < graph_input.length; i++){ 
+        let edge = graph_input[i].split("-").map(x => parseInt(x.trim())); //edge = {x1,x2}
+        //console.log(edge);
     }
+
+    // let x = 50;
+    // let y = 50;
+    // let diametro = 25;
+    // for(let v of par){
+    //     //Circulo
+    //     fill(255); 
+    //     stroke(0); 
+    //     circle(x, y, diametro);
+    //     //Texto
+    //     fill(0); 
+    //     textSize(10);
+    //     textAlign(CENTER, CENTER); 
+    //     text(v.trim(), x, y);
+    //     x+= 50;
+    // }
+    // x = 50;
+    // y += 50;
 }
